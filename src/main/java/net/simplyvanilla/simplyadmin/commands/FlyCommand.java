401200c04daa8b47e922c2e7a69a006c7fd2faf7
Emmanuel Lampe <mail@emmanuel-lampe.de>
2023-06-10 15:58:03 +0200

    refactor: use player toggle to reduce code duplication

--- src/main/java/net/simplyvanilla/simplyadmin/commands/FlyCommand.java
+++ src/main/java/net/simplyvanilla/simplyadmin/commands/FlyCommand.java
@@ -1,49 +1,35 @@
 package net.simplyvanilla.simplyadmin.commands;
 
-import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import net.simplyvanilla.simplyadmin.SimplyAdminPlugin;
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class FlyCommand implements CommandExecutor {
-    private final SimplyAdminPlugin plugin;
+public class FlyCommand extends PlayerToggleCommand implements CommandExecutor {
 
     public FlyCommand(SimplyAdminPlugin plugin) {
-        this.plugin = plugin;
+        super(plugin);
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        Player target = null;
-
-        if (args.length > 0) {
-            target = Bukkit.getPlayer(args[0]);
-            if (target == null) {
-                sender.sendMessage(this.plugin.getMessage("player-not-found",
-                    Placeholder.unparsed("name", args[0])));
-                return false;
+        togglePlayer(args, sender, new ToggleFunction() {
+            @Override
+            public boolean isToggled(Player player) {
+                return player.getAllowFlight();
             }
-        } else if (sender instanceof Player) {
-            target = (Player) sender;
-        }
 
-        if (target == null) {
-            sender.sendMessage(this.plugin.getMessage("sender-has-to-be-a-player"));
-            return false;
-        }
+            @Override
+            public void enable(Player player) {
+                player.setAllowFlight(true);
+            }
 
-        boolean allowFlight = target.getAllowFlight();
-        target.setAllowFlight(!allowFlight);
-
-        if (allowFlight) {
-            target.sendMessage(plugin.getMessage("fly-disabled"));
-        } else {
-            target.sendMessage(plugin.getMessage("fly-enabled"));
-        }
-
+            @Override
+            public void disable(Player player) {
+                player.setAllowFlight(false);
+            }
+        }, "fly-enabled", "fly-disabled");
         return true;
     }
 }
--- src/main/java/net/simplyvanilla/simplyadmin/commands/VanishCommand.java
+++ src/main/java/net/simplyvanilla/simplyadmin/commands/VanishCommand.java
@@ -1,6 +1,5 @@
 package net.simplyvanilla.simplyadmin.commands;
 
-import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import net.simplyvanilla.simplyadmin.SimplyAdminPlugin;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
@@ -8,49 +7,41 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class VanishCommand implements CommandExecutor {
-    private final SimplyAdminPlugin plugin;
-
+public class VanishCommand extends PlayerToggleCommand implements CommandExecutor {
     public VanishCommand(SimplyAdminPlugin plugin) {
-        this.plugin = plugin;
+        super(plugin);
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        Player target = null;
-
-        if (args.length > 0) {
-            target = Bukkit.getPlayer(args[0]);
-            if (target == null) {
-                sender.sendMessage(this.plugin.getMessage("player-not-found",
-                    Placeholder.unparsed("name", args[0])));
-                return false;
+        togglePlayer(args, sender, new ToggleFunction() {
+            @Override
+            public boolean isToggled(Player player) {
+                return plugin.getVanishedPlayers().contains(player.getUniqueId());
             }
-        } else if (sender instanceof Player) {
-            target = (Player) sender;
-        }
 
-        if (target == null) {
-            sender.sendMessage(this.plugin.getMessage("sender-has-to-be-a-player"));
-            return false;
-        }
-
-        if (plugin.getVanishedPlayers().contains(target.getUniqueId())) {
-            plugin.getVanishedPlayers().remove(target.getUniqueId());
-            target.sendMessage(plugin.getMessage("vanish-disabled"));
-            for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
-                onlinePlayer.showPlayer(plugin, target);
-            }
-        } else {
-            plugin.getVanishedPlayers().add(target.getUniqueId());
-            target.sendMessage(plugin.getMessage("vanish-enabled"));
-            for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
-                if (!onlinePlayer.hasPermission("simplyadmin.vanish.see")) {
-                    onlinePlayer.hidePlayer(plugin, target);
+            @Override
+            public void enable(Player player) {
+                plugin.getVanishedPlayers().add(player.getUniqueId());
+                for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+                    if (!onlinePlayer.canSee(player) || onlinePlayer.hasPermission("simplyadmin.vanish.see")) {
+                        continue;
+                    }
+                    onlinePlayer.hidePlayer(plugin, player);
                 }
             }
-        }
 
+            @Override
+            public void disable(Player player) {
+                plugin.getVanishedPlayers().remove(player.getUniqueId());
+                for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+                    if (onlinePlayer.canSee(player)) {
+                        continue;
+                    }
+                    onlinePlayer.showPlayer(plugin, player);
+                }
+            }
+        }, "vanish-enabled", "vanish-disabled");
         return true;
     }
 }
